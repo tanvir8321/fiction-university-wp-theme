b0a7a9a34028a063ffc354a71496dac817371950
tanvir8321 <tanvir8321@gmail.com>
2023-01-19 00:05:49 +0600

    html template added: home page done: html to wordpress conversion

--- functions.php
+++ functions.php
@@ -2,7 +2,11 @@
 
 function fiction_theme_files()
 {
-    wp_enqueue_style('fiction_theme_main_styles', get_stylesheet_uri());
+    wp_enqueue_script('custome-google-fonts', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
+    wp_enqueue_style('custome-google-fonts', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('fiction_theme_main_styles', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style('fiction_theme_extra_styles', get_theme_file_uri('/build/index.css'));
 }
 
 add_action('wp_enqueue_scripts', 'fiction_theme_files');
